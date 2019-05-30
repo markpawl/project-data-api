183416ce2a39467130a7e7f3fa81d6ca1854641f
wasadmin <wasadmin@10.0.1.169>
2019-05-30 14:45:46 +0000

    removed unused purchase classes, schema and data

--- rest-server/src/main/java/com/webage/service/PurchaseService.java
+++
@@ -1,9 +0,0 @@
-package com.webage.service;
-
-import com.webage.domain.Purchase;
-
-public interface PurchaseService {
-	public void savePurchase(Purchase purchase);
-	public Iterable<Purchase> findAllPurchases();
-	public Purchase findPurchaseById(long id);
-}
--- rest-server/target/classes/data.sql
+++ rest-server/target/classes/data.sql
@@ -6,9 +6,3 @@ insert into EVENTS (EVENT_CODE, TITLE, DESCRIPTION ) VALUES ('CNF001', 'All-Java
 insert into EVENTS (EVENT_CODE, TITLE, DESCRIPTION ) VALUES ('WKS002', 'Spring Boot Workshop', 'Hands-on Spring Boot Workshop' );
 insert into EVENTS (EVENT_CODE, TITLE, DESCRIPTION ) VALUES ('TRN003', 'Angular Training Course', 'Five day introductory training in Angular' );
 
-insert into PURCHASES (PRODUCT, PURCHASE_DATE, CUSTOMER_ID)
-values('Mountain Bike','2010-05-12 00:00:00.0',1);
-insert into PURCHASES (PRODUCT, PURCHASE_DATE, CUSTOMER_ID)
-values('Football','2010-04-30 00:00:00.0',2);
-insert into PURCHASES (PRODUCT, PURCHASE_DATE, CUSTOMER_ID)
-values('Kayak','2010-06-05 00:00:00.0',3);
